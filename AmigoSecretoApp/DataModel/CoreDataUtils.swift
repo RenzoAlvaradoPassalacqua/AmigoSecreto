e78c273364b3f4bf9b426330d88304ac6dd83de1
Renzo Manuel Alvarado Passalacqua <ralvarado@Mac-mini-de-ralvarado.local>
2019-02-06 15:10:09 -0500

    coredata utils

---
+++ AmigoSecretoApp/DataModel/CoreDataUtils.swift
@@ -0,0 +1,48 @@
+//
+//  CoreDataUtils.swift
+//  AmigoSecretoApp
+//
+//  Created by Renzo Manuel Alvarado Passalacqua on 2/6/19.
+//  Copyright © 2019 Renzo Manuel Alvarado Passalacqua. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+
+class CoreDataUtils{
+    
+    static var sharedInstance = CoreDataUtils()
+
+    func createNewPerson (person:Person){
+        //As we know that container is set up in the AppDelegates so we need to refer that container.
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        //We need to create a context from this container
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        //Now let’s create an entity and new user records.
+        let userEntity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        
+        let userCoreData = NSManagedObject(entity: userEntity, insertInto: managedContext)
+        
+        
+        let name : String? = person.name ?? ""
+        let email : String? = person.email ?? ""
+        let password : String? = person.password ?? ""
+        let logged : Bool? = person.logged
+        let gift : String? = person.gift ?? ""
+        let state : String? = person.state ?? ""
+        
+        userCoreData.setValue(name, forKey: "name")
+        userCoreData.setValue(email, forKey: "email")
+        userCoreData.setValue(password, forKey: "password")
+        userCoreData.setValue(logged, forKey: "logged")
+        userCoreData.setValue(gift, forKey: "gift")
+        userCoreData.setValue(state, forKey: "state")
+        
+        
+    }
+    
+    
+}
